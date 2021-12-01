1db7c2ceb82a06b730b834531296f77cc4ba9b73
Taha <tsmoujtahid@outlook.de>
2021-12-01 17:13:01 +0100

    Init

---
+++ NetSceneServer/NetSceneServer/NetSceneServerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetSceneServerApp.swift
+//  NetSceneServer
+//
+//  Created by Taha Moujtahid on 01.12.21.
+//
+
+import SwiftUI
+
+@main
+struct NetSceneServerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
